feda25bb46043f8676fa1b26f7a293725136de08
1nsp1r3 <abel.andouard@gmail.com>
2024-07-07 01:05:25 +0200

    Add pressure reading for bosch project

--- bosch/main/main.cpp
+++ bosch/main/main.cpp
@@ -4,20 +4,22 @@
 #include <i3-ble.h>
 #include <i3-adc.h>
 #include <i3-mem.h>
+#include <i3-linear.h>
 #include <i3-table.h>
 #include <i3-queue.h>
 
 #define TAG "I3-MAIN"
 
-#define GAP_SIZE 14
+#define GAP_SIZE 20
 uint8_t adv_raw_data[GAP_SIZE] = {
   0x02,0x01,0x06,                //AD1: Flags
   0x04,0x09,0x4D,0x58,0x35,      //AD2: Complete Local Name "MX5"
   0x05,0x16,0x09,0x18,0x00,0x00, //AD3: Service Data 16-bit UUID 1809 (Health Thermometer Service)
-  //0x05,0x16,0x6D,0x2A,0x14,0x00, //AD4: Service Data 16-bit UUID 2A6D (Pressure characteristic)
+  0x05,0x16,0x6D,0x2A,0x00,0x00, //AD4: Service Data 16-bit UUID 2A6D (Pressure characteristic)
 };
 
 I3Queue* temperatureQueue = new I3Queue(10);
+I3Queue* pressureQueue = new I3Queue(10);
 
 /**
  * Bosch temperature sensor
@@ -53,7 +55,22 @@ float getTemperature(){
     temperatureQueue->push(value);
     float average = temperatureQueue->average();
     float ret = i3TableGetValue(table, TABLE_SIZE, (int)average);
-    ESP_LOGI(TAG, "adc value: %d average: %.2f temperature: %.2f", value, average, ret);   
+    ESP_LOGI(TAG, "adc value: %d average: %.2f temperature: %.2f °C", value, average, ret);   
+    return ret;
+}
+
+/**
+ * Pressure reading
+ */
+float getPressure(){
+    unsigned short value = i3AdcRead(ADC_CHANNEL_6);
+    pressureQueue->push(value);
+    float average = pressureQueue->average();
+    //O PSI -> 0.5V rabaissé à 0.37V soit 425 en lecture ADC
+    //145 PSI -> 4.5V rabaissé à 3.3V soit 4095 en lecture ADC
+    float ret = i3LinearGetX(0, 425, 145, 4095, (int)average);
+    if (ret < 0) ret = 0;
+    ESP_LOGI(TAG, "adc value: %d average: %.2f pressure: %.2f PSI", value, average, ret);   
     return ret;
 }
 
@@ -68,12 +85,16 @@ extern "C" void app_main(){
   ESP_LOGI(TAG, "Bonjour :-)");
 
   i3AdcInit(ADC_UNIT_2, ADC_CHANNEL_5);
+  i3AdcInit(ADC_UNIT_2, ADC_CHANNEL_6);
   i3BleInit();
   i3BleStartAdvertising(1000, adv_raw_data, GAP_SIZE);
 
   for(;;){
     short temperature = (short)(getTemperature()*100); //BLE want 7599 for 75.99°C
     i3CopyShort(adv_raw_data, 12, &temperature); //copie 2 octets à la position 12 (AD3)
+
+    short pressure = (short)(getPressure()*100); //BLE want 7599 for 75.99 PSI
+    i3CopyShort(adv_raw_data, 18, &pressure); //copie 2 octets à la position 12 (AD3)
 
     i3BleUpdateAdvertising(adv_raw_data, GAP_SIZE);
 
